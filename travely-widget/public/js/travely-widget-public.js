f9429e4b7e9e370ce0ef341ad6edbbe84f34a1dc
Andrei Abozau <andrei@travely.ee>
2025-07-07 14:14:10 +0300

    fix: fix render by DEV-527

--- travely-widget/public/js/travely-widget-public.js
+++ travely-widget/public/js/travely-widget-public.js
@@ -3,23 +3,24 @@
         if (window.travelyWidgetInitialized) return;
         if (Array.isArray(window.travelyWidgetInstances) && window.travelyWidgetInstances.length > 0) {
             const settings = [];
-            let path = '';
-            let key = '';
+
             window.travelyWidgetInstances.forEach(function(cfg) {
-                if (cfg.path) {
-                    path = cfg.path;
-                }
-                if (cfg.key) {
-                    key = cfg.key;
-                }
                 if (cfg.mode === 'result') {
-                    window.TravelySearch.initIframe(cfg.containerId, key, path);
+                    window.TravelySearch.initIframe(cfg.containerId, cfg.key, cfg.path);
+                    return;
+                }
+                if (cfg.mode === 'search') {
+                    window.TravelySearch.initSearch(cfg.containerId, cfg.path, ['search']);
                     return;
                 }
                 settings.push({
-                    ...cfg
+                    containerId: cfg.containerId,
+                    mode: cfg.mode,
+                    pathToSearch: cfg.path,
+                    key: cfg.key,
                 });
             });
+
             window.TravelySearch.initSearchSeparate(settings);
         }
         window.travelyWidgetInitialized = true;
